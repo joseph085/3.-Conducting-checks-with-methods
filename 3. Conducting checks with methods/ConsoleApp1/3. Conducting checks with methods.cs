c3c6d45b84f49bd00d3af5108c5bafcd52233fd3
joseph085 <126801512+joseph085@users.noreply.github.com>
2023-04-02 22:55:33 +0400

    Add files via upload

---
+++ 3. Conducting checks with methods/ConsoleApp1/3. Conducting checks with methods.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+using System.Text.RegularExpressions;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Herflerin elave edilmesi 
+            Console.Write("Sexsin adi : ");
+            string PersonName = Console.ReadLine();
+            Console.WriteLine("Condition for Name :" + ExecuteName(PersonName, 20));
+
+            Console.Write("Sexsin soyadi : ");
+            string PersonLastName = Console.ReadLine();
+            Console.WriteLine("Condition for LastName :" + ExecuteName(PersonLastName, 35));
+
+            Console.Write("Sexsin Ata adi : ");
+            string PersonFatherName = Console.ReadLine();
+            Console.WriteLine("Condition for FatherName :" + ExecuteName(PersonFatherName, 20));
+
+            //Yasin daxil edilmesi 
+            Console.Write("Yasi daxil edin :");
+            int Personage = int.Parse(Console.ReadLine());
+            Console.WriteLine("Condition for PersonAge :" + ExecutePersonAge(Personage));
+
+            // Fin kodun  daxil edilmesi 
+            Console.Write("Sexsiyyet vesiqesinin FIN kodu :");
+            string FinCode = Console.ReadLine();
+            Console.WriteLine("Condition for PinCode :" + ExecuteFinCode(FinCode));
+
+            // +994 ile nomre daxil edilmesi 
+            Console.Write("Telefon nomresini daxil edin :");
+            string Number = Console.ReadLine();
+            Console.WriteLine("Position for :" + ExecutePhoneNumber(Number));
+
+            // Is Movqeyini daxil edilmesi 
+            Console.Write("Is Pozisyonunu daxil edin : ");
+            string Pozisya = Console.ReadLine();
+            Console.WriteLine("Position for :" + ExecutePosition(Pozisya));
+
+            //Maasin daxil edilmesi 
+            Console.Write("Maasi daxil edin :");
+            int maas = int.Parse(Console.ReadLine());
+            Console.WriteLine("Condition for salary :" + ExecuteSalary(maas));
+
+
+
+        }
+
+        static bool ExecuteName(string PersonName, int max)
+        {
+            if (PersonName[0] >= 'A' && PersonName[0] <= 'Z')
+            {
+                if (PersonName.Length > 2 && PersonName.Length <= max)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+
+            }
+            return false;
+        }
+
+        static bool ExecutePersonAge(int PersonAge)
+        {
+            if (PersonAge >= 18 && PersonAge <= 65)
+            {
+                return true;
+            }
+            return false;
+
+        }
+
+        static bool ExecuteFinCode(string PinCode)
+        {
+            for (int i = 0; i < PinCode.Length; i++)
+            {
+                if ((PinCode[0] >= 'A') && (PinCode[0] <= 'Z') || (PinCode[i] >= '0') && (PinCode[i] <= '9'))
+                {
+                    continue;
+                }
+                return false;
+
+
+
+            }
+            if (PinCode.Length == 7)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+
+        static bool ExecutePhoneNumber(string PhoneNumber)
+        {
+            for (int i = 0; i < PhoneNumber.Length; i++)
+            {
+                if (PhoneNumber[0] == '+' && PhoneNumber[1] == '9' && PhoneNumber[2] == '9' && PhoneNumber[3] == '4')
+                {
+                    return true;
+                }
+                return false;
+            }
+            if (PhoneNumber.Length == 13)
+            {
+                return true;
+            }
+            return false;
+
+
+
+
+        }
+
+
+        static bool ExecutePosition(string Position)
+        {
+            if (Position == "HR" || Position == "Audit" || Position == "Engineer")
+            {
+                return  true;
+            }
+            return false;
+        }
+
+        static bool ExecuteSalary (int Salary)
+        {
+            if ((Salary >= 1500) && (Salary <= 5000))
+            {
+                return true;
+
+            }else return false;
+
+        }
+    }
+}
